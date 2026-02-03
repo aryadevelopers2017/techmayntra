d86dafde6173b15506c20e87048526a1e26e48ef
aryadevelopers2017 <156194651+aryadevelopers2017@users.noreply.github.com>
2026-02-03 15:19:51 +0530

    solve problem and fix bug of the login in appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		 if(env('APP_ENV') == 'local') {
-			URL::forceScheme('https');
-		}
-		
+
+
         View::composer('*', function ($view) {
 			$data = CompanyModuleServiceProvider::module_list();
 			$view->with('module_data', $data['data'])->with('currency', $data['currency']);
